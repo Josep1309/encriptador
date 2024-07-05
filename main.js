046d484d7c6da311e4f78d28c808c828f9bfde4e
josepachecoserna <josepachecoserna@gmail.com>
2024-07-05 04:32:33 +0000

    primer manejo JS

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-// Add JS here
+document.getElementById("encriptar").addEventListener("click", function() {
+  var text = document.getElementById("myInput").value;
+  document.getElementById('result_text').innerText = "Texto ingresado: " + text;
+});
